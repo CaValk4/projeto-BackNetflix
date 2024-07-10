5bfb05cc4b9d83230164c4528afe9d2a09566e4c
CaValk4 <vitorcvk18@gmail.com>
2024-07-09 22:22:36 -0300

    ROUTES

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
-app.get("/teste", (req, res)=>{
+app.use(morgan('dev'));
+
+//LISTAR TODOS OS REGISTROS
+app.get("/", (req, res)=>{
     // regra de negocio
-    console.log('find route!')
-    res.json({mensagem: "ok"});
+    res.json({mensagem: "GET ALL RECORDS"});
+});
+//PEGAR REGISTRO A PARTIR DO ID
+app.get('/:id', (req, res)=>{
+
+    const id = req.params.id;
+    res.json({ mensagem: `GET JUST THE RECORD WITH ID: ${id}`});
 });
 
+//CRIAR UM REGISTRO
+
+app.post('/', (req, res)=>{
+
+    const body = req.body;
+    res.json({ mensagem: "POST"})
+})
+
+//EDITAR DADOS DO REGISTRO
+
+app.put('/:id', (req, res)=>{
+
+    const id = req.params.id;
+    res.json({ mensagem: `EDIT JUST THE RECORD WITH ID: ${id}`});
+})
+
+//DELETAR REGISTRO P/ ID
+
+app.delete('/:id', (req, res)=>{
+
+    const id = req.params.id;
+    res.json({ mensagem: `DELETE JUST THE RECORD WITH ID: ${id}`});
+})
 app.listen(3000,()=>{
     console.log("Servidor rodando na porta 3000");
 });
